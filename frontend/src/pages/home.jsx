01fcc50a8a183c4e64b05a7fae0f383ef00042e4
Vigneshwaran <vigneshwaranmca22@gmail.com>
2025-04-09 22:54:19 +0530

    some small edit

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const res = await axios.get("http://localhost:5000/api/products");
+        const res = await axios.get("https://ammanmedicals.onrender.com/api/products");
         setProducts(res.data);
       } catch (err) {
         console.error("❌ Failed to fetch products:", err);
